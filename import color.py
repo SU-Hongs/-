8c4f3cad159a922a75af69a3dfa3b6d61b7c9eef
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2021-08-15 16:02:35 +0800

    Add files via upload

---
+++ import color.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from pandas import ExcelFile
+Color_Path = '/Users/suhong/Downloads/基于多种复杂约束条件下的智能排产附件/订单表.xlsx'
+df = pd.read_excel (Color_Path)
+print(df)
+print
+
+
+#define a function to convert a 颜色 (string) to the color shade
+#color shade has five levels (from light to dark): 0,1,2,3,4
+def get_shade_level(color):
+    if color in ['PC透明色','本色']:
+        return 0
+    elif color in ['白色','金属色']:
+        return 1
+    elif color in ['浅灰色','浅蓝色','黄色']:
+        return 2
+    elif color == '其他':
+        return 3
+    elif color == '黑色':
+        return 4
+
+
+level = get_shade_level('浅灰色')
+print (level)
+
+                 
+    
+#define a function to store 基料 (string) as a list
+def get_base(base):
+    result = base.split('/')
+    return result
+
+base = 'PP/PE'
+print(get_base(base))
